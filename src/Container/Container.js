066e13610ef1a8ffd52b56d9eaac0f89be7039d4
Wiola-work <wiolagluza@gmail.com>
2019-11-14 12:55:51 +0100

    first commit

---
+++ src/Container/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Container.css';
+
+const Container = ()=> {
+    return (
+        <section class="section">
+            <p>Container</p>
+        </section>
+    )
+}
+
+export default Container;
