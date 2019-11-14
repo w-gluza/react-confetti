915aa1d3b73baa6cb78dcf99db1a610d7352cedb
Wiola-work <wiolagluza@gmail.com>
2019-11-14 13:16:27 +0100

    assets

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Container.css';
 
+import afternoonNavy from '../assets/afternoon-navy.svg';
+import butterscotch from '../assets/butterscotch.svg';
+import lightblue from '../assets/lightblue.svg';
+import salmonPink from '../assets/salmon-pink.svg';
+import sustainableGreen from '../assets/sustainable-green.svg';
+
 const Container = ()=> {
     return (
         <section class="section">
             <p>Container</p>
+        <img src={afternoonNavy} alt='icon'></img>
+        <img src={butterscotch} alt='icon'></img>
+        <img src={lightblue} alt='icon'></img>
+        <img src={salmonPink} alt='icon'></img>
+        <img src={sustainableGreen} alt='icon'></img>
         </section>
     )
 }
